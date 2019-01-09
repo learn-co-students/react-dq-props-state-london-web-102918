fe8eade131c02d290e2d658ceacbcb2f5d415e9e
seeduardo <romenflatiron@gmail.com>
2019-01-09 10:44:55 +0000

    Done.

--- src/MasterHog.js
+++ src/MasterHog.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import Master from './assets/master-hog.png'
 import BabyHog from './BabyHog'
-// import offspring from './db.js'
+import offspring from './db.js'
 
 export default class MasterHog extends Component {
 
@@ -14,9 +14,18 @@ export default class MasterHog extends Component {
 
 
   changeEyeColor = (e) => {
+    console.log(e.target.value)
     this.setState({
       eyeColor: e.target.value
     })
+  }
+
+  generatePiglets = () => {
+    return (
+      offspring.map((piglet) =>
+      <BabyHog pigletData={piglet} eyeColour={this.state.eyeColor} />
+      )
+    )
   }
 
 
@@ -30,19 +39,17 @@ export default class MasterHog extends Component {
         Sun<br></br>
         <input type="radio" name="eyeColor" value="glowing" onChange={this.changeEyeColor} />
         Glowing<br></br>
-      
+
         <h2>Name: Master Blaster</h2>
         <h3>Weight: 2.54 Tons</h3>
         <h3>Eye Color: {this.state.eyeColor}</h3>
-        
+
         <div id="masters-domicile">
           <img id="master-blaster" src={Master} alt="" />
         </div>
-        
+
         <ul className="hoglist">
-          <BabyHog />
-          <BabyHog />
-          <BabyHog />
+          {this.generatePiglets()}
         </ul>
 
       </div>
